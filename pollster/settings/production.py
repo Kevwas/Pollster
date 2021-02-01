02affd1c7e1ff3b451ef52dc9202bdfe462bb857
kevwas <arizakevin.dev@example.com>
2021-02-01 03:57:37 -0500

    fixing production settings

--- pollster/settings/production.py
+++ pollster/settings/production.py
@@ -2,8 +2,14 @@ from pollster.settings.base import *
 # For heroku
 import django_heroku
 import dj_database_url
+import os
 
 DEBUG = False
+
+if DEBUG:
+    SECRET_KEY = '_rl8(m5j#opso1r4h&0!8f3==%!i9m3jslar^5z5w(aw9d1nks'
+else:
+    SECRET_KEY = os.environ['SECRET_KEY']
 
 # For heroku
 ALLOWED_HOSTS = ['*']
@@ -19,3 +25,4 @@ django_heroku.settings(locals())
 # STATICFILES_DIRS = []
 
 # STATIC_ROOT = BASE_DIR / 'static'
+
