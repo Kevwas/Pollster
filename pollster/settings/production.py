56e9dfee63c1ef78c7e524f7eece7341eaa1845c
kevwas <arizakevin.dev@example.com>
2021-02-01 04:05:03 -0500

    fixing requeriments.txt

--- pollster/settings/production.py
+++ pollster/settings/production.py
@@ -6,11 +6,6 @@ import os
 
 DEBUG = False
 
-if DEBUG:
-    SECRET_KEY = '_rl8(m5j#opso1r4h&0!8f3==%!i9m3jslar^5z5w(aw9d1nks'
-else:
-    SECRET_KEY = os.environ['SECRET_KEY']
-
 # For heroku
 ALLOWED_HOSTS = ['*']
 
