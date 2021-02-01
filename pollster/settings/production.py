abaed89482cf95e656ce59faae36750a9e280d7b
kevwas <arizakevin.dev@example.com>
2021-02-01 06:00:33 -0500

    config files

--- pollster/settings/production.py
+++ pollster/settings/production.py
@@ -18,6 +18,22 @@ SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
 # For heroku
 django_heroku.settings(locals())
 
